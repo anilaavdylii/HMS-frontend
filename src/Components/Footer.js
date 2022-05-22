7a953501ecfa8e75f986048906045540ea3cd6b0
anilaavdylii <aa48857@ubt-uni.net>
2022-05-22 14:54:11 +0200

    updates

---
+++ src/Components/Footer.js
@@ -0,0 +1,128 @@
+import React, { useRef } from 'react';
+import { GrLocation } from "react-icons/gr";
+import { MdEmail } from "react-icons/md";
+import { BsPhone } from "react-icons/bs"
+import emailjs from '@emailjs/browser';
+import styled from "styled-components";
+
+
+function Footer() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+        .sendForm(
+          "service_apwiotn",
+          "template_xqxit39",
+          form.current,
+          "I9gW4BiVw65knnx0-"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            console.log("message sent");
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    };
+
+  return (
+    <footer className="page-footer font-small blue pt-4">
+    <div className="container-fluid text-center text-md-left">
+        <div className="row">
+
+            <hr className="clearfix w-100 d-md-none pb-0"/>
+
+            <div className="col-md-5 text-center py-3">
+                <ul className="list-unstyled">
+               
+                <h6><GrLocation/>Lagjja Spitalit, Prishtine 10000, Kosove</h6>
+                <a className="btn btn-outline-primary btn-xs" href="/register">Gjej rrugen</a>
+                </ul>
+
+                <ul className="list-unstyled">
+                <h6><MdEmail/>drejtoria.shskuk@rks-gov.net</h6>
+                </ul>
+
+                <ul className="list-unstyled">
+                <h6><BsPhone/>+381 38 500 600</h6>
+                </ul>
+            </div>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+            <StyledContactForm>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </StyledContactForm>
+            </div>
+        </div>
+    </div>
+
+    <div className="footer-copyright text-center py-3">© 2020 Copyright: QKUK
+    
+    </div>
+
+</footer>
+
+  )
+}
+
+export default Footer
+
+
+const StyledContactForm = styled.div`
+  width: 400px;
+  form {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    width: 100%;
+    font-size: 16px;
+    input {
+      width: 100%;
+      height: 35px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+    textarea {
+      max-width: 100%;
+      min-width: 100%;
+      width: 100%;
+      max-height: 100px;
+      min-height: 100px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+    label {
+      margin-top: 1rem;
+    }
+    input[type="submit"] {
+      margin-top: 2rem;
+      cursor: pointer;
+      background: #52B2BF;
+      color: white;
+      border: none;
+    }
+  }
+`;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+import Footer from '../Components/Footer';
+import PostCard from './PostCard';
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div className='cards'>
+      <h2>Rreth Semundjeve</h2>
+       <hr color="#4682B4" />
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <PostCard
+              src='../img/disease1.jpg'
+              name='Semundja 1'
+              category='Category2'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  '
+              // path='/services'
+            />
+             <PostCard
+              src='../img/disease1.jpg'
+              name='Semundja 1'
+              category='Category2'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  '
+              // path='/services'
+            />
+             <PostCard
+              src='../img/disease1.jpg'
+              name='Semundja 1'
+              category='Category2'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  '
+              // path='/services'
+            />           
+          </ul>
+        </div>
+      </div>
+    </div>
+    <Footer/>
+    </>
   )
 }
 
