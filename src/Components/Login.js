4d7039374e21d6737a5c2472332cd462213bda7d
anilaavdylii <aa48857@ubt-uni.net>
2022-05-22 11:53:01 +0200

    Login form validation

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 function Login() {
+
+  const initialValues = { email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } 
+    return errors;
+  };
+
   return (
     <div className="App">
       <header className='App-header'>
@@ -14,7 +52,7 @@ function Login() {
                 <Card className='card-style'>
                   <h1 className='login-heading'>Kycu</h1>
                   
-                <Form style={{margin:"30px"}}> 
+                <Form style={{margin:"30px"}} onSubmit={handleSubmit}> 
 
                   <Button variant="outline-primary" 
                   size="md"> <img width="20" height="20"
@@ -23,14 +61,24 @@ function Login() {
 
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label className='label-text'>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Shkruani email-in" />
+                    <Form.Control type="email" placeholder="Shkruani email-in"                      
+                      name="email"
+                      value={formValues.email}
+                      onChange={handleChange}
+                    />
+                     <p>{formErrors.email}</p>
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label className='label-text'>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Shkruani password-in " />
+                    <Form.Control type="password" placeholder="Shkruani password-in " 
+                      name="password"
+                      value={formValues.password}
+                      onChange={handleChange}
+                    />
+                     <p>{formErrors.password}</p>
                   </Form.Group>
-    
+                  <br/>
                   <a href="#/" className='forgot-pw'>
                    Keni harruaj fjalekalimin?
                   </a>
