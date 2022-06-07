e244c3d1c63b9319ed0bf517bd4d984983289c24
anilaavdylii <aa48857@ubt-uni.net>
2022-06-07 19:25:01 +0200

    updates

--- src/Components/Doctors.js
+++ src/Components/Doctors.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import DoctorCard from './DoctorCard';
+import doc1 from '../img/doctor1.png'; 
+import doc2 from '../img/doctor2.png';
+import team from '../img/meet-team.png';
 
 function Doctors() {
   return (
-    <div>Doctors</div>
+    <>
+
+      <div className='cards'>
+      <h4>Our professional team</h4>
+       
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items2'>
+            <DoctorCard
+              src={doc1}
+              name='Filan Fisteku'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  '
+              // path='/'
+            />
+             <DoctorCard
+              src={doc2}
+              name='Filan Fisteku'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  '
+              // path='/'
+            />
+             <DoctorCard
+              src={doc1}
+              name='Filan Fisteku'
+              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  '
+              // path='/'
+            />           
+          </ul>
+
+    
+        </div>
+
+       
+      </div>
+    </div>
+    </>
   )
 }
 
