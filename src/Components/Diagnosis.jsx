5fde18e60be2f04d26cc461735ea55e92568b34d
erakastrati <74623805+erakastrati@users.noreply.github.com>
2022-07-10 12:56:11 +0200

    added the diagnosis title and desc for each component

--- src/Components/Diagnosis.jsx
+++ src/Components/Diagnosis.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-export default function Diagnosis() {
+export default function Diagnosis({ diagnose }) {
 	return (
-		<div className="diagnosis">
-			<div className="card p-5 bg-secondary opacity-50 text-light">
-				<div className="card-body">
-					<div className="card-title fw-bold">Disease 1</div>
-					<div className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, debitis?</div>
-				</div>
-			</div>
-		</div>
+		<>
+			{diagnose &&
+				diagnose.map((d, i) => (
+					<div className="diagnosis" key={i}>
+						<div className="card p-5 bg-secondary opacity-50 text-light">
+							<div className="card-body">
+								<div className="card-title fw-bold">Diagnose</div>
+								<div className="card-text">{d.diagnose}</div>
+							</div>
+						</div>
+					</div>
+				))}
+		</>
 	);
 }
