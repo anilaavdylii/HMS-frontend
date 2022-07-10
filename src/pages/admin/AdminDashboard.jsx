d2a0a5c5c49655bdeabc5497337a6c07284e86cc
erakastrati <74623805+erakastrati@users.noreply.github.com>
2022-07-10 12:42:20 +0200

    addd the total number of registered doctors and patients in dashboard

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
 
-export default function AdminDashboard() {
+export default function AdminDashboard({ patients, doctors, posts }) {
 	return (
-		<div className="adminDashboard top container">
+		<div className="adminDashboard container-fluid top">
 			<div className="row">
-				<div className="col-2 adminDashboardLeft">
+				<div className="col-2 adminDashboardLeft ps-0">
 					<Sidebar />
 				</div>
 
@@ -15,8 +15,8 @@ export default function AdminDashboard() {
 							<img src="assets/admin/patient.png" alt="" />
 						</div>
 						<div>
-							<div>Total Patient</div>
-							<div className="text-center">250</div>
+							<div>Total Patient :</div>
+							<div className="text-center">{patients && patients.totalElements}</div>
 						</div>
 					</div>
 
@@ -25,8 +25,8 @@ export default function AdminDashboard() {
 							<img src="assets/admin/doctor.png" alt="" />
 						</div>
 						<div>
-							<div>Total Patient</div>
-							<div className="text-center">250</div>
+							<div>Total Doctor :</div>
+							<div className="text-center">{doctors && doctors.totalElements}</div>
 						</div>
 					</div>
 
@@ -35,8 +35,8 @@ export default function AdminDashboard() {
 							<img src="assets/admin/post.png" alt="" />
 						</div>
 						<div>
-							<div>Total Patient</div>
-							<div className="text-center">250</div>
+							<div>Total Post :</div>
+							<div className="text-center">{posts.totalElements ? posts.totalElements : "0"}</div>
 						</div>
 					</div>
 				</div>
--- src/pages/admin/AdminDoctors.jsx
+++ src/pages/admin/AdminDoctors.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import Sidebar from "../../components/Sidebar";
 
-export default function AdminDoctors() {
+export default function AdminDoctors({doctors}) {
 	return (
-		<div className="AdminDoctors top container">
+		<div className="AdminDoctors top container-fluid">
 			<div className="row">
-				<div className="col-2">
+				<div className="col-2 ps-0">
 					<Sidebar />
 				</div>
 				<div className="col">
 					<h1>All List of Doctors</h1>
-					<input type="text" placeholder="Search name of the doctor..." className="form-control" />
+					{/* <input type="text" placeholder="Search name of the doctor..." className="form-control" /> */}
 					<table className="table mt-5 table-hover">
 						<thead>
 							<tr className="table-secondary">
@@ -23,48 +24,34 @@ export default function AdminDoctors() {
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<td>
-									<div className="form-check">
-										<input type="checkbox" className="form-check-input" />
-									</div>
-								</td>
-								<td>Doctor 1</td>
-								<td>doctor1@gmail.com</td>
-								<td>10/10/1970</td>
-								<td>Neurologist</td>
-								<td>
-									<button className="btn-sm">See the schedule</button>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div className="form-check">
-										<input type="checkbox" className="form-check-input" />
-									</div>
-								</td>
-								<td>Doctor 2</td>
-								<td>doctor2@gmail.com</td>
-								<td>10/10/1970</td>
-								<td>Neurologist</td>
-								<td>
-									<button className="btn-sm">See the schedule</button>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div className="form-check">
-										<input type="checkbox" className="form-check-input" />
-									</div>
-								</td>
-								<td>Doctor 3</td>
-								<td>doctor3@gmail.com</td>
-								<td>10/10/1970</td>
-								<td>Neurologist</td>
-								<td>
-									<button className="btn-sm">See the schedule</button>
-								</td>
-							</tr>
+							{doctors.content ? (
+								doctors.content.map(doctor => (
+									<tr key={doctor.id}>
+										<td>
+											<div className="form-check">
+												<input type="checkbox" className="form-check-input" />
+											</div>
+										</td>
+										<td>
+											{doctor.firstName} {doctor.lastName}
+										</td>
+										<td>{doctor.email}</td>
+										<td>10/10/1970</td>
+										<td>{doctor.experiences.map(experience => experience)}</td>
+										<td>
+											<Link to="/admin-schedules">
+												<button className="btn-sm">See the schedule</button>
+											</Link>
+										</td>
+									</tr>
+								))
+							) : (
+								<tr>
+									<td className="fw-bold text-center" colSpan={6}>
+										Loading the data...
+									</td>
+								</tr>
+							)}
 						</tbody>
 					</table>
 				</div>
