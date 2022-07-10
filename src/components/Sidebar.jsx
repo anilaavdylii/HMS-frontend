5a3997a3d5c3910bad97d7168137e49b08153971
anilaavdylii <aa48857@ubt-uni.net>
2022-07-10 12:16:54 +0200

    updated navbar, sidebar and footer

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,30 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Sidebar() {
 	return (
-		<div className="sidebar ps-4 pt-3">
-			<div>
-				<img src="assets/admin/dashboard.png" alt="" /> Dashboard
-			</div>
-			<div>
-				<img src="assets/admin/patient.png" alt="" /> Patients
-			</div>
-			<div>
+		<div className="sidebar container-fluid d-flex flex-column gap-4 pt-5">
+			<Link to="/admin-dashboard">
+				<img src="assets/admin/dashboard.png" alt="" className="iconDashboard" /> Dashboard
+			</Link>
+			<Link to="/admin-patients">
+				<img src="assets/admin/patient.png" alt="" className="iconPatient" /> Patients
+			</Link>
+			<Link to="/admin-doctors">
 				<img src="assets/admin/doctor.png" alt="" /> Doctors
-			</div>
-			<div>
-				<img src="assets/admin/post.png" alt="" /> Posts
-			</div>
+			</Link>
+			<Link to="/admin-posts">
+				<i className="bi bi-newspaper iconAdmin" /> Articles
+			</Link>
+			<Link to="/admin-appointments">
+				<i className="bi bi-calendar-date-fill iconAdmin" /> Appointments
+			</Link>
+			<Link to="/admin-schedules">
+				<i className="bi bi-clock-fill iconAdmin"></i> Schedules
+			</Link>
+			<Link to="/admin-setSchedule">
+				<i className="bi bi-alarm-fill iconAdmin"></i> Set Schedule
+			</Link>
 		</div>
 	);
 }
