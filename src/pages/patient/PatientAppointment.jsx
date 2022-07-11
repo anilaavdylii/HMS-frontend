12d69f6ad9d59d49a2749570a47f3910fab4c363
erakastrati <74623805+erakastrati@users.noreply.github.com>
2022-07-11 10:26:56 +0200

    appointment of patients based on time slots of doctors

--- src/pages/patient/PatientAppointment.jsx
+++ src/pages/patient/PatientAppointment.jsx
@@ -1,23 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosInstance } from "../../config";
 
-export default function patientAppointment() {
+export default function patientAppointment({ doctors }) {
+	const [doctorId, setDoctorId] = useState("");
+	const [date, setDate] = useState("");
+	const [description, setDescription] = useState("");
+	const [error, setError] = useState("");
+	const [success, setSuccess] = useState("");
+	const [slots, setSlots] = useState("");
+	const [slotNumber, setSlotNumber] = useState("");
+
+	useEffect(() => {
+		async function handleDateTime() {
+			doctors.content && setDoctorId(doctors.content[0].id);
+			try {
+				const res = await axiosInstance.post("/slots", {
+					doctorId,
+					date,
+				});
+				setSlots(res.data);
+			} catch (err) {
+				if (err.response) {
+					console.log(err.response);
+				}
+			}
+		}
+		handleDateTime();
+	}, [date, doctorId]);
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		setError("");
+		setSuccess("");
+
+		try {
+			const res = await axiosInstance.post("/users/appointment", {
+				doctorId,
+				date,
+				description,
+				slot: { slotNumber: slotNumber },
+			});
+			res && setSuccess("Your appointment has been sent successfully!");
+			console.log(res);
+		} catch (err) {
+			if (err.response) {
+				setError("You failed to submit the appointment!");
+				console.log(err.response);
+			}
+		}
+	}
+
 	return (
-		<div className="patientAppointment container top">
+		<div className="patientAppointment container top text-center">
 			<h1>Make an Appointment</h1>
-			<label className="form-label">Choose your doctor:</label>
-			<select name="doctor" className="d-block form-control ms-0">
-				<option value="A">Dr. A</option>
-				<option value="B">Dr. B</option>
-				<option value="C">Dr. C</option>
-			</select>
-			<label className="form-label mt-4">Input your date and time appointment:</label>
-			<div>
-				<input type="date" placeholder="Appointment Date" className="ms-0 mt-0" />
-				<input type="text" placeholder="Appointment Time" className="mt-0" />
-			</div>
-			<textarea name="description" cols="30" rows="10" placeholder="Description"></textarea>
-			<br />
-			<button className="mt-3 ms-5">Apply Appointment</button>
+			<form onSubmit={handleSubmit}>
+				<label className="form-label">Choose your doctor:</label>
+				<div className="d-flex justify-content-center">
+					<select name="doctor" className="form-control w-25" required value={doctorId} onChange={(e) => setDoctorId(e.target.value)}>
+						{doctors.content ? (
+							doctors.content.map((doctor) => (
+								<option value={doctor.id} key={doctor.id}>
+									{doctor.firstName} {doctor.lastName}
+								</option>
+							))
+						) : (
+							<option>Loading the data...</option>
+						)}
+					</select>
+				</div>
+				<label className="form-label mt-4">Input your date and time appointment:</label>
+				<div className="d-flex justify-content-center">
+					<input
+						type="date"
+						placeholder="Appointment Date"
+						className="mt-0 form-control w-25"
+						required
+						value={date}
+						onChange={(e) => setDate(e.target.value)}
+					/>
+					<select name="time" className="form-control w-25" required value={slotNumber} onChange={(e) => setSlotNumber(e.target.value)}>
+						{slots &&
+							slots.map((slot, i) => (
+								<option value={slot.slotNumber} key={i}>
+									{slot.timeSlot}
+								</option>
+							))}
+					</select>
+				</div>
+				<div className="d-flex justify-content-center">
+					<textarea
+						className="form-control w-75"
+						name="description"
+						cols="30"
+						rows="10"
+						placeholder="Description"
+						required
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
+					/>
+				</div>
+				<br />
+				<button className="mt-3 ms-5">Apply Appointment</button>
+				<div className="error">{error}</div>
+				<div className="success">{success}</div>
+			</form>
 		</div>
 	);
 }
