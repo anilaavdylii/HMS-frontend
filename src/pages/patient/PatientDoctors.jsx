d364bee5633a947ae7231cebb2e3577e3c22ed23
anilaavdylii <aa48857@ubt-uni.net>
2022-07-11 12:52:52 +0200

    added list of registered doctors -patient page

--- src/pages/patient/PatientDoctors.jsx
+++ src/pages/patient/PatientDoctors.jsx
@@ -1,29 +1,22 @@
 import React from "react";
 import Doctor from "../../components/Doctor";
 
-export default function PatientDoctors() {
+export default function PatientDoctors({ doctors }) {
 	return (
-		<div className="container-fluid top">
+		<div className="top">
 			<div className="text-center">
-				<img src="assets/patient/image21.png" alt="" />
+				<img src="assets/patient/doctors.jpeg" alt="" className="imgDoctors" />
 			</div>
 
 			<div className="container">
 				<h1>Our Professional Team</h1>
-				<div className="text-center">
-					<input type="text" className="patientDoctorsInput" />
-				</div>
+				{/* <div className="d-flex justify-content-center position-relative">
+					<input type="text" className="patientDoctorsInput form-control position-absolut m-0 rounded-4" />
+					<i className="bi bi-search" />
+				</div> */}
 
 				<div className="doctorsTeam d-flex justify-content-between flex-wrap">
-					<Doctor />
-					<Doctor />
-					<Doctor />
-					<Doctor />
-					<Doctor />
-					<Doctor />
-					<Doctor />
-					<Doctor />
-					<Doctor />
+					<Doctor doctors={doctors} />
 				</div>
 			</div>
 		</div>
